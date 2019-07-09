4d20116421179ba020ab28030ffc3d80b21095c4
alsterium <new033@gmail.com>
2019-07-09 23:21:38 +0900

    file-write implement

--- src/multi_resolution_processing.cpp
+++ src/multi_resolution_processing.cpp
@@ -3,6 +3,7 @@
 //#include "pch.h"
 #include <vector>
 #include <iostream>
+#include <atltime.h>
 #include <opencv2\opencv.hpp>
 #include "property.h"
 #include "full_serch_l.h"
@@ -18,17 +19,18 @@ int main(int argc, char* argv[]) {
 	cout << "---->main()\n";
 	cout << "";
 
-	if (argc != 4) {
-		cout << "usage: [MultiResLevel] [DstRes] [Nbr]";
-		exit(0);
+	if (argc != 5) {
+		cout << "usage: [file] [MultiResLevel] [DstRes] [Nbr]";
+		exit(-1);
 	}
 
-	int MultiResLevel = atoi(argv[1]);
-	int DstRes = atoi(argv[2]);
-	int Nbr = atoi(argv[3]);
+	int MultiResLevel = atoi(argv[2]);
+	int DstRes = atoi(argv[3]);
+	int Nbr = atoi(argv[4]);
+	cv::String filepath = argv[1];
 
 	cv::Mat src_image;
-	src_image = cv::imread("./texture_o_icon.jpg");
+	src_image = cv::imread(filepath);
 	vector<cv::Mat> src_img_vector;
 	vector<cv::Mat> dst_img_vector;
 
@@ -54,7 +56,7 @@ int main(int argc, char* argv[]) {
 	}
 	catch(cv::Exception& e){
 		cerr << e.what() << endl;
-		exit(1);
+		exit(-2);
 	}
 	
 
@@ -62,12 +64,9 @@ int main(int argc, char* argv[]) {
 	cout << "-->initialize each image.\n";
 	for (auto &e : dst_img_vector) { cv::randu(e, cv::Scalar(0, 0, 0), cv::Scalar(256, 256, 256));}
 
-	//最低解像度の画像合成
-	//cout << "--->lowest resolution image synthesis is running...\n";
-	F_Property prop(Nbr);
-	//fullserch_L(src_img_vector[(int)(src_img_vector.size() - 1)], dst_img_vector[(int)(dst_img_vector.size() - 1)], prop);
-
 	//各解像度ごとに一つ下のレベルの解像度を参照して画像合成
+
+	F_Property prop(Nbr);
 	cout << "--->multi resolution synthesis is begin!!\n";
 	synthesis_multi(src_img_vector, dst_img_vector, prop);
 
@@ -86,16 +85,20 @@ int main(int argc, char* argv[]) {
 
 	for (int i = 0; i < dst_img_vector.size(); i++) {
 		cv::String windowName = "result";
+		CTime time = CTime::GetTickCount();
+		cv::String time_s = time.Format("%Y%m%d_%H%M%S");
+		cv::String filename = "[MR-"+to_string(MultiResLevel) + "][DR-"+to_string(DstRes)+"][Nb-"+to_string(Nbr)+"][Dpth-"+to_string(i)+"]_"+time_s+".png";
 		try {
 			cv::imshow(windowName + to_string(i), dst_img_vector[i]);
+			cv::imwrite("./result/"+filename, dst_img_vector[i]);
 		}
 		catch(cv::Exception& e){
 			cerr << e.what() << endl;
-			exit(1);
+			exit(-3);
 		}
 	}
 
 	cout << "<----end main()\n";
-	cv::waitKey(0);
+	cv::waitKey(1000);
 }
 
