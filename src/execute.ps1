1ef0a6041ba828f45c4942bdc60edc8ac260b067
alsterium <new033@gmail.com>
2019-07-17 16:54:01 +0900

    powershell script added

---
+++ src/execute.ps1
@@ -0,0 +1,32 @@
+Write-Host $Args[0]
+
+.\multiResSynthesis.exe $Args[0] 2 150 3
+.\multiResSynthesis.exe $Args[0] 3 150 3
+.\multiResSynthesis.exe $Args[0] 4 150 3
+
+.\multiResSynthesis.exe $Args[0] 2 300 3
+.\multiResSynthesis.exe $Args[0] 3 300 3
+.\multiResSynthesis.exe $Args[0] 4 300 3
+
+.\multiResSynthesis.exe $Args[0] 2 500 3
+.\multiResSynthesis.exe $Args[0] 3 500 3
+.\multiResSynthesis.exe $Args[0] 4 500 3
+
+\.multiResSynthesis.exe $Args[0] 3 150 3
+.\multiResSynthesis.exe $Args[0] 3 150 5
+.\multiResSynthesis.exe $Args[0] 3 150 7
+
+.\multiResSynthesis.exe $Args[0] 3 300 3
+.\multiResSynthesis.exe $Args[0] 3 300 5
+.\multiResSynthesis.exe $Args[0] 3 300 7
+
+.\multiResSynthesis.exe $Args[0] 3 500 3
+.\multiResSynthesis.exe $Args[0] 3 500 5
+.\multiResSynthesis.exe $Args[0] 3 500 7
+
+.\multiResSynthesis.exe $Args[0] 3 100 3
+.\multiResSynthesis.exe $Args[0] 3 200 3
+.\multiResSynthesis.exe $Args[0] 3 300 3
+.\multiResSynthesis.exe $Args[0] 3 400 3
+.\multiResSynthesis.exe $Args[0] 3 500 3
+
---
+++ src/run.ps1
@@ -0,0 +1,5 @@
+./execute.ps1 295.jpg
+./execute.ps1 425.jpg
+./execute.ps1 479.jpg
+./execute.ps1 726.jpg
+
